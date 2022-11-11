cfcc5cfaa104e7baced95276edf27bc22b6ec273
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-11-11 09:03:51 +0800

    limit hashtag to 50 chars

--- utils/hashtagFormatter.js
+++ utils/hashtagFormatter.js
@@ -6,12 +6,19 @@ export default function hashtagFormatter(hashtagsRaw) {
     let hashtagsArray = hashtagsModified.split(" ").filter(hashtag => hashtag.trim() != '');
     //remove duplicate hashtags
     hashtagsArray = hashtagsArray.filter((hashtag, index) => hashtagsArray.indexOf(hashtag) === index);
-
     //Limit the hashtags to only 5
     if (hashtagsArray.length > 5)
     {
         hashtagsArray.splice(5);
     }
+    //Limit each hashtag to only 50 characters
+    hashtagsArray.forEach((hashtag) => {
+        if (hashtag.length > 50)
+        {
+            hashtag.splice(50);
+        }
+    });
+    
     //Use 
     // hashtagsNormalizedString = hashtagsArray.join(" ");
     //To create a string of normalized hashtags seperated by spaces
