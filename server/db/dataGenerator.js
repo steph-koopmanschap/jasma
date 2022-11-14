9a3edf863197b95b21945389cdc513929c42a589
l4lilul3lo <logankirk989@gmail.com>
2022-11-14 11:49:36 -0800

    add userMetaData generator

--- server/db/dataGenerator.js
+++ server/db/dataGenerator.js
@@ -77,4 +77,31 @@ function userInfoGen(user_id) {
     return { user_id, ...objectify(methods) };
 }
 
-module.exports = { userGen, userInfoGen };
+function userMetaDataGen(user_id, user_role) {
+    const methods = {
+        last_login_date() {
+            return new Date().toISOString();
+        },
+        account_creation_date() {
+            return new Date().toISOString();
+        },
+        isVerfied_email() {
+            if (user_role === "admin" || user_role === "mod") {
+                return true;
+            }
+
+            if (user_role === "normal") {
+                return Math.random() < 0.5;
+            }
+
+            return false;
+        },
+        last_ip4() {
+            return faker.internet.ipv4();
+        }
+    };
+
+    return { user_id, user_role, ...objectify(methods) };
+}
+
+module.exports = { userGen, userInfoGen, userMetaDataGen };
--- server/db/userGenerator.js
+++ server/db/userGenerator.js
@@ -1,6 +1,8 @@
-const { userGen, userInfoGen } = require("./dataGenerator");
+const { userGen, userInfoGen, userMetaDataGen } = require("./dataGenerator");
 
 const user = userGen();
 const userInfo = userInfoGen(user.user_id);
+const userMetaData = userMetaDataGen(user.user_id, "normal");
 console.log("user", user);
 console.log("userInfo", userInfo);
+console.log("userMetaData", userMetaData);
