4223bcf8d8017d682b2c7b89b2064a640efec754
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-11-14 10:08:07 +0800

    add delete comment controller

---
+++ server/controllers/comment/deleteComment.js
@@ -0,0 +1,21 @@
+const pool = require("../../lib/dbConnect.js");
+
+//Delete a post
+async function deleteComment(commentID) {
+    let result = await pool.query(
+        `
+        DELETE FROM comments
+        WHERE comment_id = $1
+        `,
+        [commentID]);
+
+    if (!result) {
+        let err = new Error(`Error: SQL query failed.`);
+        console.log(err);
+        return err;
+    }
+    //post deleted
+    return commentID;
+}
+
+module.exports = deleteComment;
