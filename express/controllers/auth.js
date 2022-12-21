dfc71eb114fd9c2815084d686118e39b0aa11e98
steph-koopmanschap <stephkoopmanschap@protonmail.com>
2022-12-21 13:48:33 +0800

    fix issue #11 User can login with any password.

--- express/controllers/auth.js
+++ express/controllers/auth.js
@@ -33,7 +33,7 @@ async function login(req, res) {
         return res.json({ success: false, message: "Email or password is incorrect." });
     }
 
-    const isCorrectPassword = UserPassword.compare(email, password);
+    const isCorrectPassword = await UserPassword.compare(email, password);
     if (!isCorrectPassword) {
         return res.json({ success: false, message: "Email or password is incorrect." });
     }
