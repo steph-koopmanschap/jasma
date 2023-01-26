8aeba48e0dc210c0d42cdf4017b236da04d6dc0f
l4lilul3lo <logankirk989@gmail.com>
2023-01-26 10:59:05 -0800

    add multipartHandler middleware

--- express/routes/postsRouter.js
+++ express/routes/postsRouter.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const isAuth = require("../middleware/isAuth.js");
-const { createPost, getUserPosts, getLatestPosts } = require("../controllers/posts.js")
-
+const { createPost, getUserPosts, getLatestPosts } = require("../controllers/posts.js");
+const { multipartHandler } = require("../middleware/multipartHandler");
 
 const postsRouter = express.Router();
 
-postsRouter.post('/createPost', isAuth, createPost);
+postsRouter.post("/createPost", isAuth, multipartHandler, createPost);
 // api/posts/getUserPosts?user_id=UUID&limit=50
-postsRouter.get('/getUserPosts', getUserPosts);
+postsRouter.get("/getUserPosts", getUserPosts);
 // api/posts/getLatestPosts?limit=50
-postsRouter.get('/getLatestPosts', getLatestPosts);
+postsRouter.get("/getLatestPosts", getLatestPosts);
 
 module.exports = { postsRouter };
