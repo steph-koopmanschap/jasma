5ed5564d7dc86e9d0c8afb38a8e571202379c774
François Florent <francois.florent.01@gmail.com>
2023-05-18 21:34:18 -0400

    Another fix ... lol

--- backend/django_server/settings.py
+++ backend/django_server/settings.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles', # Not needed if frontend
+    'django.contrib.staticfiles',
     'django_extensions',
     'api'
 ]
@@ -211,7 +211,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/' # Not needed if frontend
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
